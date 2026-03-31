21ac0064d5039545fcd1e532d4b4953d2a1e7e55
Hrlmine <168060751+Hrlmine@users.noreply.github.com>
2026-03-31 21:11:13 +0700

    Create BuildingTask.js for building tasks

---
+++ src/tasks/BuildingTask.js
@@ -0,0 +1,15 @@
+// BuildingTask.js
+
+class BuildingTask {
+    constructor() {
+        // Initialize task properties here
+    }
+
+    execute() {
+        // Define how to execute a building task here
+    }
+
+    // Additional methods can be defined here
+}
+
+module.exports = BuildingTask;
