1fe3253156ac25cd4a946e08303039bb40971bac
Hrlmine <168060751+Hrlmine@users.noreply.github.com>
2026-03-31 21:11:23 +0700

    Create MiningTask.js for mining tasks

---
+++ src/tasks/MiningTask.js
@@ -0,0 +1,15 @@
+// MiningTask.js
+
+class MiningTask {
+    constructor() {
+        // Initialization code here
+    }
+    startMining() {
+        // Code to start the mining task
+    }
+    stopMining() {
+        // Code to stop the mining task
+    }
+}
+
+module.exports = MiningTask;
